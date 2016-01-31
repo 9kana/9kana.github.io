dbc13ca5d7daf3bf45bc39d977fb5e8063c389f9
9kana <kana.menstruation@gmail.com>
2016-01-31 15:37:16 +0900

    upload

--- js/touch.js
+++ js/touch.js
@@ -1,6 +1,21 @@
 // DOMロード
 $(function(){
 
+// 緯度・軽度
+var lon;
+var lat;
+
+  // GPS
+  navigator.geolocation.getCurrentPosition(function(evt){
+    // 緯度
+    var lon = evt.coords.longitude;
+    //  軽度
+    var lat = evt.coords.latitude;
+  },
+  function(error){
+    console.log('GPS取れなかったよー');
+  });
+
   // タッチイベント
   $('#ball').on('touchstart',function(){
     $('#touchResult li').eq(0).text('touchstartしたよ');
@@ -12,6 +27,9 @@ $(function(){
 
   $('#ball').on('touchend',function(){
     $('#touchResult li').eq(2).text('touchendしたよ');
+
+    $('#geo li').eq(0).text('緯度' + lon);
+    $('#geo li').eq(1).text('軽度' + lat);
   });
 
 };)

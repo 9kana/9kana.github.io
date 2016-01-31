c567e9bb17de8f54df7826275e25e5791eb9c527
9kana <kana.menstruation@gmail.com>
2016-01-31 14:57:53 +0900

    upload

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -10,6 +10,10 @@ $(function(){
     $('#device li').eq(0).text('加速度X: ' + x);
     $('#device li').eq(1).text('加速度Y: ' + y);
     $('#device li').eq(2).text('加速度Z: ' + z);
+
+    if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15){
+      $('#result').text('降ったよ');
+    }
   }
-  
+
 });

a359a7f4be3a53e0b99f6aadac1e8bbdcaf9871d
9kana <kana.menstruation@gmail.com>
2016-02-10 18:54:07 +0900

    log

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -1,5 +1,6 @@
 $(function(){
   // GPS
+  console.log('DOM読み込んだよ');
   navigator.geolocation.getCurrentPosition(function(pos){
     var lat = pos.coords.latitude;
     var lon = pos.coords.longitude;

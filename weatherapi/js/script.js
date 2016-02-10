6fee3ef1d836930c8832c0e6df35bd3db13657f8
9kana <kana.menstruation@gmail.com>
2016-02-10 19:43:17 +0900

    up

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -6,5 +6,22 @@ $(function(){
     var lon = pos.coords.longitude;
 
     console.log(lat,lon);
+
+    //定数
+    const apikey = '2f9278cf2b5d932086da421cb6cf84c5';
+
+    var url = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon +'&appid=' + apikey;
+
+    $.ajax({
+      url:url,
+      dataType:'json'
+    }).done(function(data){
+      console.log(data);
+
+    }).fail(function(){
+      console.log('ajax error!!!');
+    });
+
+
   });
 });
